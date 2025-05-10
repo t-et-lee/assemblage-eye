5a9f41c2625d00b679430f6615f2a78eb6dcef0e
Labyrinth Lee <gomletghost@gmail.com>
2025-05-10 00:05:25 -0700

    close game after all text traversed (provisional measure)

--- scripts/main.gd
+++ scripts/main.gd
@@ -19,6 +19,9 @@ func next_text():
 		#Then read their dialogue and send it over.
 		newline = currenttext.get_line()
 		textbox.change_text(newline)
+		#Close the game if there's nothing left.
+		if currenttext.eof_reached():
+			get_tree().quit()
 		#Then read the miscellaneous commands
 		newline = currenttext.get_line()
 		match newline:
@@ -27,6 +30,7 @@ func next_text():
 			"fx rain":
 				layers.fx.queue_free()
 				layers.fx = load("res://scenes/layers/fx_rain.tscn").instantiate()
+				layers.add_child(layers.fx)
 
 func load_text(filenum: int):
 	self.currenttext = FileAccess.open(list_texts[filenum],FileAccess.READ)
